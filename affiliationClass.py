85ef9809af52bcd3084acde9e121807422870388
Allison Rodenbaugh <alro7383@colorado.edu>
2018-05-17 10:34:42 -0600

    Delete affiliationClass.py

--- affiliationClass.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# For use in survey.py
-
-# list of affiliation options for acronym
-
-class AffiliationOptions():
-
-    # dict of options
-    options = ['-- choose --', 'N/A', 'LASP-general', 'NASA-general', 'Operational Software', 'OASIS', 'Mission-Name', 'Multi-Mission', 'AIM', 'GOES-R', 'Kepler', 'MMS', 'Maven','QuikSCAT', 'SME', 'SORCE', 'SNOE', 'TSIS', '~Custom~']
